bb1ce039b65e622bf6eb85b5d2700bb729ec7acc
Pathrova <rohrerz090844339@gmail.com>
2026-05-30 04:32:37 -0700

    2nd

--- about.php
+++
@@ -1,8 +0,0 @@
-<?php include 'header.php'; ?>
-<section class="hero-section" style="padding: 150px 0 100px;">
-    <div class="container">
-        <h1 class="hero-title" style="text-align:center;">About <span>Sherbuys</span></h1>
-        <p class="hero-description" style="margin: 0 auto; text-align:center;">We are passionate about delivering the future of technology directly to your hands. Sherbuys curates only the most premium, high-performance devices for our uncompromising customers.</p>
-    </div>
-</section>
-<?php include 'footer.php'; ?>
